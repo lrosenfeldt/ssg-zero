815de3da38f49122b0cf3b542a53ba19d76bbf02
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-08-20 12:40:06 +0200

    feat: add logger

---
+++ src/logger.test.ts
@@ -0,0 +1,64 @@
+import assert from 'node:assert/strict'
+import { describe, it, mock } from 'node:test'
+
+import { ConsoleLogger, LogLevel } from './logger.js'
+
+describe('logger.ts', () => {
+	describe('ConsoleLogger', () => {
+		const debugMessage = 'What is love?'
+		const infoMessage = "Baby, don't hurt me"
+		const warnMessage = "Don't hurt me"
+		const errorMessage = 'No more'
+
+		describe('configurable log level', () => {
+			const table = [
+				{
+					level: LogLevel.Debug,
+					messages: [
+						debugMessage,
+						infoMessage,
+						warnMessage,
+						errorMessage,
+					],
+				},
+				{
+					level: LogLevel.Info,
+					messages: [infoMessage, warnMessage, errorMessage],
+				},
+				{ level: LogLevel.Warn, messages: [warnMessage, errorMessage] },
+				{ level: LogLevel.Error, messages: [errorMessage] },
+			]
+
+			table.forEach(({ level, messages }) => {
+				it(`only runs for logs on ${level} and below`, t => {
+					const logger = new ConsoleLogger(level)
+					const consoleLog = t.mock.method(console, 'log')
+
+					logger.debug(debugMessage)
+					logger.info(infoMessage)
+					logger.warn(warnMessage)
+					logger.error(errorMessage)
+
+					assert.equal(consoleLog.mock.callCount(), messages.length)
+				})
+			})
+		})
+
+		it('has a sane log format', t => {
+			const referenceDate = new Date('2020-04-20T06:09:11')
+
+			const consoleLog = t.mock.method(console, 'log')
+			t.mock.method(global, 'Date', function () {
+				return referenceDate
+			})
+
+			const logger = new ConsoleLogger(LogLevel.Error)
+			logger.error('unicorn')
+
+			assert.equal(
+				consoleLog.mock.calls[0].arguments[0],
+				'2020/04/20 06:09:11 ERROR unicorn',
+			)
+		})
+	})
+})
---
+++ src/logger.ts
@@ -0,0 +1,68 @@
+export interface Logger {
+	debug(message: string): void
+	error(message: string): void
+	info(message: string): void
+	warn(message: string): void
+}
+
+export enum LogLevel {
+	Debug = 'DEBUG',
+	Error = 'ERROR',
+	Info = 'INFO',
+	Warn = 'WARN',
+}
+
+export class ConsoleLogger implements Logger {
+	private static noop = () => void 0
+
+	constructor(maxLogLevel: LogLevel) {
+		switch (maxLogLevel) {
+			case LogLevel.Debug:
+				break
+			case LogLevel.Info:
+				this.debug = ConsoleLogger.noop
+				break
+			case LogLevel.Warn:
+				this.debug = ConsoleLogger.noop
+				this.info = ConsoleLogger.noop
+				break
+			case LogLevel.Error:
+				this.debug = ConsoleLogger.noop
+				this.info = ConsoleLogger.noop
+				this.warn = ConsoleLogger.noop
+				break
+		}
+	}
+
+	debug(message: string): void {
+		this.log(LogLevel.Debug, message)
+	}
+	error(message: string): void {
+		this.log(LogLevel.Error, message)
+	}
+	info(message: string): void {
+		this.log(LogLevel.Info, message)
+	}
+	warn(message: string): void {
+		this.log(LogLevel.Warn, message)
+	}
+
+	private log(logLevel: LogLevel, message: string): void {
+		const date = new Date()
+		const formattedDate = [
+			date.getFullYear().toString(),
+			// javascript getMonth is starting a zero
+			// why tho?
+			(date.getMonth() + 1).toString().padStart(2, '0'),
+			date.getDate().toString().padStart(2, '0'),
+		].join('/')
+
+		const time = [
+			date.getHours().toString().padStart(2, '0'),
+			date.getMinutes().toString().padStart(2, '0'),
+			date.getSeconds().toString().padStart(2, '0'),
+		].join(':')
+
+		console.log(`${formattedDate} ${time} ${logLevel} ${message}`)
+	}
+}
