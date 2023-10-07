e5735e292e598eb6c9365451983632f4d7eb9e5a
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-10-07 12:39:31 +0200

    refactor: improve naming and make tests exhaustive

--- ssg-zero/src/cli/parse_args.test.ts
+++ ssg-zero/src/cli/parse_args.test.ts
@@ -1,153 +1,242 @@
 import assert from 'node:assert/strict';
 import { describe as suite, test } from 'node:test';
-import { Token, TokenType, Tokenizer } from './parse_args.js';
+import { Token, Parsed, Parser, SchemaRegistry, Schema } from './parse_args.js';
 
 suite('parse_args.ts', function () {
+	const schema: Schema = {
+		globals: {
+			happy: { type: 'string', short: 'H', default: 'yes' },
+			unicorns: { type: 'number', short: 'u' },
+			switch: { type: 'boolean' },
+			'log-level': { type: 'number', default: 3 },
+			speed: { type: 'number', short: 's' },
+			intermediate: { type: 'boolean', short: 'i', default: false },
+			awesome: { type: 'boolean', short: 'A' },
+			config: { type: 'string', short: 'c', default: 'config.json' },
+			'output-dir': { type: 'string', short: 'o' },
+		},
+		commands: {
+			dance: {
+				style: { type: 'string', default: 'breakdance', short: 'S' },
+				fast: { type: 'boolean' },
+			},
+		},
+	};
+	suite('SchemaRegistry', function () {
+		const registry = new SchemaRegistry(schema);
+		suite('isCommand', function () {
+			test('validates a defined command', function () {
+				assert.equal(
+					registry.isCommand('dance'),
+					true,
+					'dance should be a known command, but returned false',
+				);
+			});
+			test('returns false for an unknown command', function () {
+				assert.equal(
+					registry.isCommand('unknown-gibberish'),
+					false,
+					"unknown-gibberish should't be valid command, but returned true",
+				);
+			});
+		});
+		suite('getDefaults', function () {
+			test('only returns the global defaults when given no command', function () {
+				const defaults = registry.getDefaults();
+
+				assert.deepEqual(defaults, {
+					config: 'config.json',
+					happy: 'yes',
+					intermediate: false,
+					'log-level': 3,
+				});
+			});
+			test('returns the global defaults and command specific defaults when given a command', function () {
+				const defaults = registry.getDefaults('dance');
+
+				assert.deepEqual(defaults, {
+					style: 'breakdance',
+					config: 'config.json',
+					happy: 'yes',
+					intermediate: false,
+					'log-level': 3,
+				});
+			});
+		});
+	});
 	suite('Tokenizer', function () {
 		suite('tokenize', function () {
-			const tokenizer = new Tokenizer({
-				globals: {
-					happy: { type: 'string', short: 'H' },
-					unicorns: { type: 'number', short: 'u' },
-					switch: { type: 'boolean' },
-					'log-level': { type: 'number' },
-					speed: { type: 'number', short: 's' },
-					intermediate: { type: 'boolean', short: 'i' },
-					awesome: { type: 'boolean', short: 'A' },
-					config: { type: 'string', short: 'c' },
-					'output-dir': { type: 'string', short: 'o' },
-				},
-				commands: {
-          'dance': {},
-        },
-			});
+			const tokenizer = new Parser(schema);
 
 			test('tokenizes everything after the terminator as positionals', function () {
 				const args = ['--', '--unicorn', '--unit=fm', '-cats'];
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Positional,
+						type: Parsed.Positional,
 						index: 1,
 						value: '--unicorn',
 					},
 					{
-						type: TokenType.Positional,
+						type: Parsed.Positional,
 						index: 2,
 						value: '--unit=fm',
 					},
 					{
-						type: TokenType.Positional,
+						type: Parsed.Positional,
 						index: 3,
 						value: '-cats',
 					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
 			test('tokenizes long options and inline values', function () {
 				const args = ['--switch', '--happy=yes', '--speed', '4269'];
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'switch',
 						value: true,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 1,
 						name: 'happy',
 						value: 'yes',
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 2,
 						name: 'speed',
 						value: 4269,
 					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
 			test('tokenizes short options', function () {
 				const args = ['-u', '4269', '-i'];
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'unicorns',
 						value: 4269,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 2,
 						name: 'intermediate',
 						value: true,
 					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
 			test('tokenizes short options with values', function () {
-				const args = ['-icflags', '-s', '3141', '-H', '9:00', '-omain'];
+				const args = [
+					'-icflags',
+					'-s',
+					'3141',
+					'-H',
+					'9:00',
+					'-omain',
+					'dance',
+					'-Sdisco',
+				];
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'intermediate',
 						value: true,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'config',
 						value: 'flags',
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 1,
 						name: 'speed',
 						value: 3141,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 3,
 						name: 'happy',
 						value: '9:00',
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 5,
 						name: 'output-dir',
 						value: 'main',
 					},
+					{
+						type: Parsed.Command,
+						index: 6,
+						name: 'dance',
+					},
+					{
+						type: Parsed.Option,
+						index: 7,
+						name: 'style',
+						value: 'disco',
+					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
-			test('tokenizes a command and positionals positional', function () {
-				const args = ['-Hyes', 'dance', '--switch'];
+			test('tokenizes a command and positionals', function () {
+				const args = [
+					'-Hyes',
+					'dance',
+					'--switch',
+					'--fast',
+					'arg1',
+					'something',
+				];
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'happy',
 						value: 'yes',
 					},
 					{
-						type: TokenType.Command,
+						type: Parsed.Command,
 						index: 1,
 						name: 'dance',
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 2,
 						name: 'switch',
 						value: true,
 					},
+					{
+						type: Parsed.Option,
+						index: 3,
+						name: 'fast',
+						value: true,
+					},
+					{
+						type: Parsed.Positional,
+						index: 4,
+						value: 'arg1',
+					},
+					{
+						type: Parsed.Positional,
+						index: 5,
+						value: 'something',
+					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
 			test('tokenizes a full list of options', function () {
 				const args = [
@@ -157,55 +246,69 @@ suite('parse_args.ts', function () {
 					'4',
 					'-iAs',
 					'9000',
+					'dance',
+					'-S',
+					'moshpit',
 					'--',
 					'-isAu',
 				];
 
 				const expectedTokens: Token[] = [
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 0,
 						name: 'unicorns',
 						value: NaN,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 1,
 						name: 'switch',
 						value: true,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 2,
 						name: 'log-level',
 						value: 4,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 4,
 						name: 'intermediate',
 						value: true,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 4,
 						name: 'awesome',
 						value: true,
 					},
 					{
-						type: TokenType.Option,
+						type: Parsed.Option,
 						index: 4,
 						name: 'speed',
 						value: 9000,
 					},
 					{
-						type: TokenType.Positional,
+						type: Parsed.Command,
+						index: 6,
+						name: 'dance',
+					},
+					{
+						type: Parsed.Option,
 						index: 7,
+						name: 'style',
+						value: 'moshpit',
+					},
+					{
+						type: Parsed.Positional,
+						index: 10,
 						value: '-isAu',
 					},
 				];
 
-				assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+				assert.deepEqual(tokenizer.parse(args), expectedTokens);
 			});
 			suite('report problems', function () {
 				test('reports problems for inline values', function () {
@@ -215,123 +318,124 @@ suite('parse_args.ts', function () {
 					];
 					const expectedTokens: Token[] = [
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 0,
 							message:
 								"Tried to set a value for unknown option '--unknown-endeavours'",
 						},
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 1,
 							message: "Got unexpected value for '--switch'",
 						},
 					];
 
-					assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
 				});
 				test('reports problems for long options', function () {
 					const args = ['--say-what', '--log-level'];
 
 					const expectedTokens: Token[] = [
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 0,
 							message: "Found unknown option '--say-what'",
 						},
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 1,
 							message:
 								"Expected value for '--log-level' but reached end of arguments",
 						},
 					];
 
-					assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
 				});
-        test('report problems for a short', function () {
+				test('report problems for a short', function () {
 					const args = ['-l', '-u'];
 					const expectedTokens: Token[] = [
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 0,
 							message: "Found unknown alias '-l'",
 						},
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 1,
 							message:
 								"Expected value for '-u' but reached end of arguments",
 						},
 					];
 
-					assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
-        });
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
+				});
 				test('reports problems for a group of shorts', function () {
 					const args = ['-nHi', '-Au'];
 					const expectedTokens: Token[] = [
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 0,
 							message:
 								"Got invalid group '-nHi', contains unknown alias '-n'",
 						},
 						{
-							type: TokenType.Option,
+							type: Parsed.Option,
 							index: 1,
 							name: 'awesome',
 							value: true,
 						},
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 1,
 							message:
 								"Expected value for '-u' at the end of group but reached end of arguments",
 						},
 					];
 
-					assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
 				});
-        test('reports problems for a command', function () {
-          const args = ['-s42', '--switch', 'worktree'];
-          const expectedTokens: Token[] = [
-            {
-              type: TokenType.Option,
-              index: 0,
-              name: 'speed',
-              value: 42,
-            }, {
-              type: TokenType.Option,
-              index: 1,
-              name: 'switch',
-              value: true,
-            },
-            {
-              type: TokenType.Problem,
-              index: 2,
-              message: "Found unknown command 'worktree'",
-            }
-          ];
+				test('reports problems for a command', function () {
+					const args = ['-s42', '--switch', 'worktree'];
+					const expectedTokens: Token[] = [
+						{
+							type: Parsed.Option,
+							index: 0,
+							name: 'speed',
+							value: 42,
+						},
+						{
+							type: Parsed.Option,
+							index: 1,
+							name: 'switch',
+							value: true,
+						},
+						{
+							type: Parsed.Problem,
+							index: 2,
+							message: "Found unknown command 'worktree'",
+						},
+					];
 
-          assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
-        });
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
+				});
 				test('reports a type error for a number', function () {
 					const args = ['-sfast', '--log-level', 'silent'];
 					const expectedTokens: Token[] = [
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 0,
 							message:
 								"Expected a number for option '--speed' but got 'fast'",
 						},
 						{
-							type: TokenType.Problem,
+							type: Parsed.Problem,
 							index: 1,
 							message:
 								"Expected a number for option '--log-level' but got 'silent'",
 						},
 					];
 
-					assert.deepEqual(tokenizer.tokenize(args), expectedTokens);
+					assert.deepEqual(tokenizer.parse(args), expectedTokens);
 				});
 			});
 		});
--- ssg-zero/src/cli/parse_args.ts
+++ ssg-zero/src/cli/parse_args.ts
@@ -22,58 +22,65 @@ export type Schema = {
 	commands: Record<string, CommandSchema>;
 };
 
-export enum TokenType {
+export enum Parsed {
 	Option = 'OPTION',
 	Problem = 'PROBLEM',
 	Positional = 'POSITIONAL',
 	Command = 'COMMAND',
 }
 
-export type Token =
+export type ParsedArg = 
 	| {
-			type: TokenType.Positional;
+			type: Parsed.Positional;
 			index: number;
 			value: string;
 	  }
 	| {
-			type: TokenType.Option;
+			type: Parsed.Option;
 			index: number;
 			name: string;
 			value: any;
 	  }
 	| {
-			type: TokenType.Positional;
+			type: Parsed.Positional;
 			index: number;
 			value: string;
 	  }
 	| {
-			type: TokenType.Problem;
-			index: number;
-			message: string;
-	  }
-	| {
-			type: TokenType.Command;
+			type: Parsed.Command;
 			index: number;
 			name: string;
 	  };
 
-export class Tokenizer {
+
+export type ParsedProblem = {
+  type: Parsed.Problem,
+  index: number;
+  message: string;
+}
+
+export type Token = ParsedArg | ParsedProblem;
+
+export class Parser {
 	private args: string[] = [];
 	private arg: string = '\0';
 	private position: number = 0;
 	private reachedTerminator: boolean = false;
-  private command: string | undefined = undefined;
+	private schemaRegistry: SchemaRegistry;
+	private command: string | undefined = undefined;
 
-	constructor(private schema: Schema) {}
+	constructor(schema: Schema) {
+		this.schemaRegistry = new SchemaRegistry(schema);
+	}
 
-	tokenize(args: string[]): Token[] {
+	parse(args: string[]): Token[] {
 		this.setup(args);
 
 		const tokens: Array<Token | Token[]> = [];
 		for (; this.arg !== '\0'; this.nextArg()) {
 			if (this.reachedTerminator) {
 				tokens.push({
-					type: TokenType.Positional,
+					type: Parsed.Positional,
 					index: this.position - 1,
 					value: this.arg,
 				});
@@ -88,30 +95,23 @@ export class Tokenizer {
 			} else if (this.arg.startsWith('-') && this.arg.length > 2) {
 				tokens.push(this.getTokensForShortOptionGroup());
 			} else if (this.command === undefined) {
-        tokens.push(this.getTokenForCommand());
+				tokens.push(this.getTokenForCommand());
 			} else {
 				tokens.push({
-					type: TokenType.Positional,
+					type: Parsed.Positional,
 					index: this.position - 1,
 					value: this.arg,
 				});
-      }
+			}
 		}
 
-		return tokens.flatMap(tokenOrList => {
-			if (Array.isArray(tokenOrList)) {
-				return tokenOrList.map(token => this.bindIfOption(token));
-			} else {
-				return this.bindIfOption(tokenOrList);
-			}
-		});
+		return tokens.flat(1);
 	}
 
-	private bindIfOption(token: Token): Token {
-		if (token.type !== TokenType.Option) {
-			return token;
-		}
-		const schema = this.schema.globals[token.name];
+	private bindOption(
+		token: Extract<Token, { type: Parsed.Option }>,
+		schema: FlagSchema,
+	): Token {
 		if (schema.type !== 'number') {
 			return token;
 		}
@@ -124,7 +124,7 @@ export class Tokenizer {
 		const asNumber = Number(value);
 		if (Number.isNaN(asNumber)) {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: token.index,
 				message: `Expected a number for option '--${token.name}' but got '${token.value}'`,
 			};
@@ -141,98 +141,108 @@ export class Tokenizer {
 		this.arg = this.args[this.position++];
 	}
 
-  private getTokenForCommand(): Token {
-    if (!(this.arg in this.schema.commands)) {
-      return {
-        type: TokenType.Problem,
-        index: this.position - 1,
-        message: `Found unknown command '${this.arg}'`,
-      }
-    }
+	private getTokenForCommand(): Token {
+		if (!this.schemaRegistry.isCommand(this.arg)) {
+			return {
+				type: Parsed.Problem,
+				index: this.position - 1,
+				message: `Found unknown command '${this.arg}'`,
+			};
+		}
 
-    this.command = this.arg;
-    return {
-      type: TokenType.Command,
-      index: this.position - 1,
-      name: this.command,
-    }
-  }
+		this.command = this.arg;
+		return {
+			type: Parsed.Command,
+			index: this.position - 1,
+			name: this.command,
+		};
+	}
 
 	private getTokenForInlineValue(): Token {
 		const indexOfEqualSign = this.arg.indexOf('=');
 		const arg = this.arg.slice(2, indexOfEqualSign);
 
-		if (!(arg in this.schema.globals)) {
+		const schema = this.schemaRegistry.find(arg, this.command);
+		if (schema === undefined) {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Tried to set a value for unknown option '--${arg}'`,
 			};
 		}
 
-		const schema = this.schema.globals[arg];
 		if (schema.type === 'boolean') {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Got unexpected value for '--${arg}'`,
 			};
 		}
 
-		return {
-			type: TokenType.Option,
-			index: this.position - 1,
-			name: arg,
-			value: this.arg.slice(indexOfEqualSign + 1),
-		};
+		return this.bindOption(
+			{
+				type: Parsed.Option,
+				index: this.position - 1,
+				name: arg,
+				value: this.arg.slice(indexOfEqualSign + 1),
+			},
+			schema,
+		);
 	}
 
 	private getTokenForLongOption(): Token {
 		const arg = this.arg.slice(2);
-		if (!(arg in this.schema.globals)) {
+		const schema = this.schemaRegistry.find(arg, this.command);
+		if (schema === undefined) {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Found unknown option '--${arg}'`,
 			};
 		}
 
-		const schema = this.schema.globals[arg];
 		if (schema.type === 'boolean') {
-			return {
-				type: TokenType.Option,
-				index: this.position - 1,
-				name: arg,
-				value: true,
-			};
+			return this.bindOption(
+				{
+					type: Parsed.Option,
+					index: this.position - 1,
+					name: arg,
+					value: true,
+				},
+				schema,
+			);
 		}
 
 		this.nextArg();
 		if (this.arg === '\0') {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Expected value for '--${arg}' but reached end of arguments`,
 			};
 		}
 
 		const value = this.arg;
-		return {
-			type: TokenType.Option,
-			index: this.position - 2,
-			name: arg,
-			value,
-		};
+		return this.bindOption(
+			{
+				type: Parsed.Option,
+				index: this.position - 2,
+				name: arg,
+				value,
+			},
+			schema,
+		);
 	}
 
 	private getTokenForShortOption(): Token {
 		const short = this.arg[1];
-		const schemaEntry = Object.entries(this.schema.globals).find(
-			({ 1: s }) => s.short && s.short === short,
+		const schemaEntry = this.schemaRegistry.findEntryByShort(
+			short,
+			this.command,
 		);
 		if (schemaEntry === undefined) {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Found unknown alias '-${short}'`,
 			};
@@ -241,80 +251,102 @@ export class Tokenizer {
 		const [name, schema] = schemaEntry;
 
 		if (schema.type === 'boolean') {
-			return {
-				type: TokenType.Option,
-				index: this.position - 1,
-				name,
-				value: true,
-			};
+			return this.bindOption(
+				{
+					type: Parsed.Option,
+					index: this.position - 1,
+					name,
+					value: true,
+				},
+				schema,
+			);
 		}
 
 		this.nextArg();
 		if (this.arg === '\0') {
 			return {
-				type: TokenType.Problem,
+				type: Parsed.Problem,
 				index: this.position - 1,
 				message: `Expected value for '-${short}' but reached end of arguments`,
 			};
 		}
 
 		const value = this.arg;
-		return {
-			type: TokenType.Option,
-			index: this.position - 2,
-			name,
-			value,
-		};
+		return this.bindOption(
+			{
+				type: Parsed.Option,
+				index: this.position - 2,
+				name,
+				value,
+			},
+			schema,
+		);
 	}
 
 	private getTokensForShortOptionGroup(): Token[] {
 		const tokens: Token[] = [];
 		const group = this.arg.slice(1);
 		for (let i = 0, char = group[i]; i < group.length; char = group[++i]) {
-			const schemaEntry = Object.entries(this.schema.globals).find(
-				([, s]) => s.short && s.short === char,
+			const schemaEntry = this.schemaRegistry.findEntryByShort(
+				char,
+				this.command,
 			);
 			if (schemaEntry === undefined) {
 				tokens.push({
-					type: TokenType.Problem,
+					type: Parsed.Problem,
 					index: this.position - 1,
-          message: `Got invalid group '${this.arg}', contains unknown alias '-${char}'`
+					message: `Got invalid group '${this.arg}', contains unknown alias '-${char}'`,
 				});
 				break;
 			}
 
 			const [name, schema] = schemaEntry;
 			if (schema.type === 'boolean') {
-				tokens.push({
-					type: TokenType.Option,
-					index: this.position - 1,
-					name,
-					value: true,
-				});
+				tokens.push(
+					this.bindOption(
+						{
+							type: Parsed.Option,
+							index: this.position - 1,
+							name,
+							value: true,
+						},
+						schema,
+					),
+				);
 			} else if (i < group.length - 1) {
-				tokens.push({
-					type: TokenType.Option,
-					index: this.position - 1,
-					name,
-					value: group.slice(i + 1),
-				});
+				tokens.push(
+					this.bindOption(
+						{
+							type: Parsed.Option,
+							index: this.position - 1,
+							name,
+							value: group.slice(i + 1),
+						},
+						schema,
+					),
+				);
 				break;
 			} else {
 				this.nextArg();
 				const value = this.arg;
 				if (value === '\0') {
 					tokens.push({
-						type: TokenType.Problem,
+						type: Parsed.Problem,
 						index: this.position - 1,
 						message: `Expected value for '-${char}' at the end of group but reached end of arguments`,
 					});
 				} else {
-					tokens.push({
-						type: TokenType.Option,
-						index: this.position - 2,
-						name,
-						value,
-					});
+					tokens.push(
+						this.bindOption(
+							{
+								type: Parsed.Option,
+								index: this.position - 2,
+								name,
+								value,
+							},
+							schema,
+						),
+					);
 				}
 			}
 		}
@@ -328,7 +360,64 @@ export class Tokenizer {
 		this.arg = '\0';
 		this.nextArg();
 
-    this.command = undefined;
+		this.command = undefined;
 		this.reachedTerminator = false;
+	}
+}
+
+export class SchemaRegistry {
+	constructor(private schema: Schema) {}
+
+	isCommand(command: string): boolean {
+		return command in this.schema.commands;
+	}
+
+	getDefaults(command?: string): Record<string, any> {
+		const defaults: Record<string, any> = {};
+		for (const name in this.schema.globals) {
+			const flagSchema = this.schema.globals[name];
+			if (flagSchema?.default !== undefined) {
+				defaults[name] = flagSchema.default;
+			}
+		}
+
+		if (command === undefined) {
+			return defaults;
+		}
+
+		const commandSchema = this.schema.commands[command];
+		for (const name in commandSchema) {
+			const flagSchema = commandSchema[name];
+			if (flagSchema?.default !== undefined) {
+				defaults[name] = flagSchema.default;
+			}
+		}
+
+		return defaults;
+	}
+
+	find(name: string, command?: string): FlagSchema | undefined {
+		if (command !== undefined && name in this.schema.commands[command]) {
+			return this.schema.commands[command][name];
+		}
+		if (name in this.schema.globals) {
+			return this.schema.globals[name];
+		}
+		return undefined;
+	}
+
+	findEntryByShort(
+		short: string,
+		command?: string,
+	): [name: string, schema: FlagSchema] | undefined {
+		if (command !== undefined) {
+			const schemaEntry = Object.entries(
+				this.schema.commands[command],
+			).find(([, s]) => s.short && s.short === short);
+			if (schemaEntry !== undefined) return schemaEntry;
+		}
+		return Object.entries(this.schema.globals).find(
+			([, s]) => s.short && s.short === short,
+		);
 	}
 }
