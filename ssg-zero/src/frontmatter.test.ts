4353eeb953e8798053e039c0594959316e01ee3c
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-10-15 12:59:30 +0200

    test: use less nesting

--- ssg-zero/src/frontmatter.test.ts
+++ ssg-zero/src/frontmatter.test.ts
@@ -36,9 +36,8 @@ at line 32
 			assert.equal(result?.data, undefined);
 		});
 	});
-	suite('given invalid frontmatter', function () {
-		test('ignores frontmatter if preceeded by empty lines', function () {
-			const text = `\
+	test('ignores frontmatter if preceeded by empty lines', function () {
+		const text = `\
 
 
 {
@@ -46,35 +45,38 @@ at line 32
 }
 <main>Look at me!</main>
 `;
-			const result = parse(text);
-			assert.equal(result?.data, undefined);
-		});
-		test('passes through syntax errors from JSON.parse', function () {
-			const text = `\
+		const result = parse(text);
+		assert.equal(result?.data, undefined);
+	});
+
+	test('passes through syntax errors from JSON.parse for invalid frontmatter', function () {
+		const text = `\
 {
   "color": "red",
   "hex": 0xFF0000
 }
 <p>Oh no!</p>
 `;
-			assert.throws(parse.bind(null, text), SyntaxError);
-		});
-		test('throws on unexpected end of json', function () {
-			const text = `\
+		assert.throws(parse.bind(null, text), SyntaxError);
+	});
+
+	test('throws on unexpected end of json for invalid frontmatter', function () {
+		const text = `\
 {
   "unicorn": "awesome",
   "creatures": ["dragon", "goblin", "zombie"],
 <p>Oh no!</p>
 `;
-			assert.throws(
-				parse.bind(null, text),
-				new UnexpectedEndOfJsonError(text, 0),
-			);
-		});
+		assert.throws(
+			parse.bind(null, text),
+			new UnexpectedEndOfJsonError(text, 0),
+		);
 	});
-	suite('given valid frontmatter', function () {
-		test('parses content and fronmatter', async function (t) {
-			const text = `\
+});
+
+suite('given valid frontmatter', function () {
+	test('parses content and fronmatter', async function (t) {
+		const text = `\
 {
   "number": 69,
   "bool": false,
@@ -85,45 +87,44 @@ at line 32
   }
 }
 Actual content`;
-			let result: ParserResult;
-			assert.doesNotThrow(() => {
-				result = parse(text);
-			});
-			await t.test('strips the frontmatter from the text', function () {
-				assert.equal(result.content, 'Actual content');
-			});
-			await t.test('contains the correct data', function () {
-				const data = result.data;
-				assert.deepEqual(data, {
-					number: 69,
-					bool: false,
-					text: 'The quick whatever does the thing.',
-					list: [1, 2, 4, 8, 16],
-					nested: {
-						abc: 123,
-					},
-				});
+		let result: ParserResult;
+		assert.doesNotThrow(() => {
+			result = parse(text);
+		});
+		await t.test('strips the frontmatter from the text', function () {
+			assert.equal(result.content, 'Actual content');
+		});
+		await t.test('contains the correct data', function () {
+			const data = result.data;
+			assert.deepEqual(data, {
+				number: 69,
+				bool: false,
+				text: 'The quick whatever does the thing.',
+				list: [1, 2, 4, 8, 16],
+				nested: {
+					abc: 123,
+				},
 			});
 		});
-		test('parses empty frontmatter and content', async function(t) {
-			const text = `\
+	});
+	test('parses empty frontmatter and content', async function (t) {
+		const text = `\
 {
 }
 <p>Someday everything will be alright</p>
 `;
-			let result: ParserResult;
-			assert.doesNotThrow(() => {
-				result = parse(text);
-			});
-			await t.test('strips frontmatter from the text', function () {
-				assert.equal(
-					result.content,
-					'<p>Someday everything will be alright</p>\n',
-				);
-			});
-			await t.test('contains empty data', function () {
-				assert.deepEqual(result.data, {});
-			});
+		let result: ParserResult;
+		assert.doesNotThrow(() => {
+			result = parse(text);
+		});
+		await t.test('strips frontmatter from the text', function () {
+			assert.equal(
+				result.content,
+				'<p>Someday everything will be alright</p>\n',
+			);
+		});
+		await t.test('contains empty data', function () {
+			assert.deepEqual(result.data, {});
 		});
 	});
 });
