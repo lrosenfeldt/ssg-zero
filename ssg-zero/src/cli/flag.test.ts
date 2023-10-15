7d32488e5465ca38accce9f472882dd24232fd0f
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-10-15 14:07:30 +0200

    refactor: simplify codebase by merging flag and decorator files - its not that cool to have everything all over the place

--- ssg-zero/src/cli/flag.test.ts
+++ ssg-zero/src/cli/flag.test.ts
@@ -1,30 +1,65 @@
 import assert from 'node:assert/strict';
-import test from 'node:test';
-import { Command } from './flag.js';
-import { boolean, number, string } from './decorator.js';
+import { describe as suite, test } from 'node:test';
+import { Command, typedFlag, boolean, number, string } from './flag.js';
 
-test('reports the usage based on the internal schema', function () {
-	class Dev extends Command {
-    @boolean({
-      description: 'Show version information',
-    })
-    version?: boolean;
-
-		@boolean({
-			short: 'd',
-			description: 'Only print out what will been generated',
-		})
-		dryRun?: boolean;
-
-		@number({ short: 'p', description: 'Port to serve build output on' })
-		port: number = 4269;
-
-    @string({ description: 'Caching strategy to use on files'}) 
-    cachingMethod: string = 'advanced-btree5'
+suite('typedFlag', function () {
+	// custom flag type
+	function icedTea(value: string) {
+		return value;
 	}
-	const dev = new Dev('Develop a static site');
+	const contextMock: ClassFieldDecoratorContext<any, any> & {
+		name: string;
+	} = {
+		name: 'coldDrink',
+		addInitializer() {
+			return;
+		},
+	} as any;
 
-	const expectedUsage = `\
+	test('sets the decorator name based on the flagType', function () {
+		const decorator = typedFlag(icedTea, {});
+		assert.equal(decorator.name, 'icedTeaFlagDecorator');
+	});
+	test('sets the name for the field initializer based on the type and field name', function () {
+		const fieldInitializer = typedFlag(icedTea, {})(undefined, contextMock);
+
+		assert.equal(fieldInitializer!.name, 'onInitColdDrinkAsIcedTea');
+	});
+	test('sets the name for the initializer based on the type and field name', function (t) {
+		const addInitializerMock = t.mock.method(contextMock, 'addInitializer');
+		typedFlag(icedTea, {})(undefined, contextMock);
+		const initializer = addInitializerMock.mock.calls[0].arguments[0];
+
+		assert.equal(initializer.name, 'onBeforeInitColdDrinkAsIcedTea');
+	});
+});
+
+suite('decorators', function () {
+	test('reports the usage based on the internal schema', function () {
+		class Dev extends Command {
+			@boolean({
+				description: 'Show version information',
+			})
+			version?: boolean;
+
+			@boolean({
+				short: 'd',
+				description: 'Only print out what will been generated',
+			})
+			dryRun?: boolean;
+
+			@number({
+				short: 'p',
+				description: 'Port to serve build output on',
+			})
+			port: number = 4269;
+
+			@string({ description: 'Caching strategy to use on files' })
+			cachingMethod: string = 'advanced-btree5';
+		}
+		const dev = new Dev('Develop a static site');
+
+		const expectedUsage = `\
 Usage: ssg-zero dev [OPTIONS]
 Develop a static site
 
@@ -35,14 +70,14 @@ Options:
       --caching-method <string>  Caching strategy to use on files (default "advanced-btree5")
 `;
 
-	assert.equal(dev.usage('ssg-zero'), expectedUsage);
-});
+		assert.equal(dev.usage('ssg-zero'), expectedUsage);
+	});
 
-test('converts the class name to kebab-case in usage', function () {
-	class DoIt extends Command {}
-	const doIt = new DoIt('DOOO IT!');
+	test('converts the class name to kebab-case in usage', function () {
+		class DoIt extends Command {}
+		const doIt = new DoIt('DOOO IT!');
 
-	const expectedUsage = `\
+		const expectedUsage = `\
 Usage: app do-it [OPTIONS]
 DOOO IT!
 
@@ -50,5 +85,6 @@ Options:
 
 `;
 
-	assert.equal(doIt.usage('app'), expectedUsage);
+		assert.equal(doIt.usage('app'), expectedUsage);
+	});
 });
--- ssg-zero/src/cli/flag.ts
+++ ssg-zero/src/cli/flag.ts
@@ -1,4 +1,11 @@
-import { toKebabCase } from "../util/string.js";
+import { toKebabCase, toPascalCase } from '../util/string.js';
+
+type ClassNamedFieldDecoratorContext<This, Value> = ClassFieldDecoratorContext<
+	This,
+	Value
+> & { name: string };
+
+type Clearify<T> = { [K in keyof T]: T[K] } & {};
 
 export type FlagType = (value: string) => any;
 
@@ -9,20 +16,7 @@ export type FlagSchema = {
 	default?: any;
 };
 
-export const numberType: FlagType = function number(value) {
-	if (value.toLowerCase() === 'NaN') {
-		return NaN;
-	}
-	const asNumber = Number(value);
-	if (Number.isNaN(asNumber)) {
-		return new Error(`Given '${value}' is not a valid number`);
-	}
-	return asNumber;
-};
-
-export const stringType: FlagType = function string(value) {
-	return value;
-};
+type FlagConfig = Clearify<Omit<FlagSchema, 'valueType' | 'default'>>;
 
 export class Command {
 	private schema: Record<string, FlagSchema> = {};
@@ -39,9 +33,9 @@ export class Command {
 		const lines: Array<[alias: string, description: string]> = [];
 		let aliasesColumnLength = 0;
 		for (const name in this.schema) {
-      const line = this.formatFlagSchema(name)
+			const line = this.formatFlagSchema(name);
 			lines.push(line);
-      aliasesColumnLength = Math.max(line[0].length, aliasesColumnLength);
+			aliasesColumnLength = Math.max(line[0].length, aliasesColumnLength);
 		}
 
 		const optionsUsage = lines
@@ -66,11 +60,12 @@ ${optionsUsage}
 	private formatFlagSchema(
 		name: string,
 	): [aliases: string, description: string] {
-    const schema = this.schema[name];
-    // aliases
-    const shortPrefix = schema.short ? `-${schema.short}, ` : '    ';
-    const typeSuffix = schema.valueType === null ? '' : ` <${schema.valueType.name}>`;
-    // description
+		const schema = this.schema[name];
+		// aliases
+		const shortPrefix = schema.short ? `-${schema.short}, ` : '    ';
+		const typeSuffix =
+			schema.valueType === null ? '' : ` <${schema.valueType.name}>`;
+		// description
 		let defaultSuffix = '';
 		if (schema.default !== undefined) {
 			defaultSuffix =
@@ -81,4 +76,84 @@ ${optionsUsage}
 		const description = `${schema.description ?? ''}${defaultSuffix}`;
 		return [shortPrefix + `--${name}` + typeSuffix, description];
 	}
+}
+
+export function typedFlag<BaseValue>(
+	flagType: FlagSchema['valueType'],
+	config: FlagConfig,
+): <This extends Command, Value extends BaseValue>(
+	value: undefined,
+	context: ClassNamedFieldDecoratorContext<This, Value>,
+) => ((this: This, value: Value) => Value) | void {
+	const typeName = flagType === null ? 'boolean' : flagType.name;
+
+	function decorator<This extends Command, Value extends BaseValue>(
+		_: undefined,
+		context: ClassNamedFieldDecoratorContext<This, Value>,
+	) {
+		const flagName = toKebabCase(context.name);
+		function onBeforeInit(this: This): void {
+			this['schema'][flagName] = Object.assign<
+				FlagConfig,
+				Pick<FlagSchema, 'valueType'>
+			>(config, { valueType: flagType });
+		}
+
+		setFunctionName(
+			onBeforeInit,
+			`onBeforeInit${toPascalCase(context.name)}As${toPascalCase(
+				typeName,
+			)}`,
+		);
+
+		context.addInitializer(onBeforeInit);
+
+		function onInit(this: This, value: Value) {
+			if (value !== undefined) {
+				this['schema'][flagName].default = value;
+			}
+			return value;
+		}
+
+		setFunctionName(
+			onInit,
+			`onInit${toPascalCase(context.name)}As${toPascalCase(typeName)}`,
+		);
+
+		return onInit;
+	}
+
+	setFunctionName(decorator, `${typeName}FlagDecorator`);
+
+	return decorator;
+}
+
+export const boolean = (typedFlag<boolean | undefined>).bind(null, null);
+export const number = (typedFlag<number | undefined>).bind(
+	null,
+	function number(value) {
+		if (value.toLowerCase() === 'NaN') {
+			return NaN;
+		}
+		const asNumber = Number(value);
+		if (Number.isNaN(asNumber)) {
+			return new Error(`Given '${value}' is not a valid number`);
+		}
+		return asNumber;
+	},
+);
+export const string = (typedFlag<string | undefined>).bind(
+	null,
+	function string(value) {
+		return value;
+	},
+);
+
+function setFunctionName(fn: Function, name: string): void {
+	Object.defineProperty(fn, 'name', {
+		enumerable: false,
+		writable: false,
+		configurable: true,
+		value: name,
+	});
 }
