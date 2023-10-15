18772da519366b18a3b1efd2e570a61a92a35d3a
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-10-15 13:59:54 +0200

    chore: add missing util files

---
+++ ssg-zero/src/util/string.ts
@@ -0,0 +1,42 @@
+export function capitalize(str: string): string {
+	if (str === '') {
+		return str;
+	}
+	return str[0].toUpperCase() + str.slice(1);
+}
+
+export function toPascalCase(str: string): string {
+	if (str === '') {
+		return str;
+	}
+  let pascalCased = str[0].toUpperCase();
+  for (let i = 1, char = str[i]; i < str.length; char = str[++i]) {
+    if (i === str.length - 1) {
+      pascalCased += char.toLowerCase();
+    } else if (char === '_' || char === '-') {
+      pascalCased += str[++i].toUpperCase(); 
+    } else {
+      pascalCased += char;
+    }
+  }
+  return pascalCased;
+}
+
+export function toKebabCase(str: string): string {
+  if (str === '') {
+    return str;
+  }
+	let kebabCased = str[0].toLowerCase();
+  for (let i = 1, char = str[i]; i < str.length; char = str[++i]) {
+    if (i === str.length - 1) {
+      kebabCased += char.toLowerCase();
+    } else if (char === '_') {
+      kebabCased += '-' + str[++i].toLowerCase(); 
+    } else if (char === char.toUpperCase()) {
+      kebabCased += '-' + char.toLowerCase();
+    } else {
+      kebabCased += char;
+    }
+  }
+	return kebabCased;
+}
