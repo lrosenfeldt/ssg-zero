2c99e1fa6118ae76f40de57f965fa37cbbd7ddb1
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-10-15 13:05:44 +0200

    test: reduce nesting, prefer functions over lambdas

--- ssg-zero/src/logger.test.ts
+++ ssg-zero/src/logger.test.ts
@@ -3,66 +3,52 @@ import { describe as suite, test } from 'node:test';
 
 import { ConsoleLogger, LogLevel } from './logger.js';
 
-suite('logger.ts', () => {
-	suite('ConsoleLogger', () => {
-		const debugMessage = 'What is love?';
-		const infoMessage = "Baby, don't hurt me";
-		const warnMessage = "Don't hurt me";
-		const errorMessage = 'No more';
+suite('ConsoleLogger', function () {
+	const debugMessage = 'What is love?';
+	const infoMessage = "Baby, don't hurt me";
+	const warnMessage = "Don't hurt me";
+	const errorMessage = 'No more';
 
-		suite('configurable log level', () => {
-			const table = [
-				{
-					level: LogLevel.Debug,
-					messages: [
-						debugMessage,
-						infoMessage,
-						warnMessage,
-						errorMessage,
-					],
-				},
-				{
-					level: LogLevel.Info,
-					messages: [infoMessage, warnMessage, errorMessage],
-				},
-				{ level: LogLevel.Warn, messages: [warnMessage, errorMessage] },
-				{ level: LogLevel.Error, messages: [errorMessage] },
-			];
+	const table = [
+		{
+			level: LogLevel.Debug,
+			messages: [debugMessage, infoMessage, warnMessage, errorMessage],
+		},
+		{
+			level: LogLevel.Info,
+			messages: [infoMessage, warnMessage, errorMessage],
+		},
+		{ level: LogLevel.Warn, messages: [warnMessage, errorMessage] },
+		{ level: LogLevel.Error, messages: [errorMessage] },
+	];
 
-			table.forEach(({ level, messages }) => {
-				test(`only runs for logs on ${level} and below`, t => {
-					const logger = new ConsoleLogger(level);
-					const consoleLog = t.mock.method(
-						console,
-						'log',
-						() => void 0,
-					);
-
-					logger.debug(debugMessage);
-					logger.info(infoMessage);
-					logger.warn(warnMessage);
-					logger.error(errorMessage);
-
-					assert.equal(consoleLog.mock.callCount(), messages.length);
-				});
-			});
-		});
-
-		test('has a sane log format', t => {
-			const referenceDate = new Date('2020-04-20T06:09:11');
-
+	for (const { level, messages } of table) {
+		test(`only runs for logs on ${level} and below`, function (t) {
+			const logger = new ConsoleLogger(level);
 			const consoleLog = t.mock.method(console, 'log', () => void 0);
-			t.mock.method(global, 'Date', function () {
-				return referenceDate;
-			});
 
-			const logger = new ConsoleLogger(LogLevel.Error);
-			logger.error('unicorn');
+			logger.debug(debugMessage);
+			logger.info(infoMessage);
+			logger.warn(warnMessage);
+			logger.error(errorMessage);
 
-			assert.equal(
-				consoleLog.mock.calls[0].arguments[0],
-				'2020/04/20 06:09:11 ERROR unicorn',
-			);
+			assert.equal(consoleLog.mock.callCount(), messages.length);
 		});
+	}
+	test('has a sane log format', function (t) {
+		const referenceDate = new Date('2020-04-20T06:09:11');
+
+		const consoleLog = t.mock.method(console, 'log', () => void 0);
+		t.mock.method(global, 'Date', function () {
+			return referenceDate;
+		});
+
+		const logger = new ConsoleLogger(LogLevel.Error);
+		logger.error('unicorn');
+
+		assert.equal(
+			consoleLog.mock.calls[0].arguments[0],
+			'2020/04/20 06:09:11 ERROR unicorn',
+		);
 	});
 });
