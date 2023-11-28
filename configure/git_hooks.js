3830faa4a36e7788b02062db9b280719f6a1f3d8
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-11-28 22:37:44 +0100

    feat: allow update of git hooks from within the script itself

--- configure/git_hooks.js
+++ configure/git_hooks.js
@@ -2,16 +2,19 @@ import { writeFileSync } from 'node:fs';
 
 const HOOK = `\
 #!/bin/sh
-npm run format
+
+files=$(git status --porcelain=v2 | awk '/\\.ts|json|js|md|css$/ {print $9}')
+echo $files
+npm exec prettier -- --check $files
 `;
 
 const PRE_COMMIT_PATH = './.git/hooks/pre-commit';
 
-function installHook() {
+function installHook(config) {
 	try {
 		writeFileSync(PRE_COMMIT_PATH, HOOK, {
 			encoding: 'utf-8',
-			flag: 'wx',
+			flag: config.flag,
 			mode: 0o755,
 		});
 	} catch (err) {
@@ -20,4 +23,21 @@ function installHook() {
 	console.log(`Successfully installed pre-commit hook for formatting.`);
 }
 
-installHook();
+function run(args) {
+	const config = { flag: 'wx' };
+
+	for (let arg = args[0]; arg !== undefined; arg = args.shift()) {
+		switch (arg) {
+			case '--allow-update':
+				config.flag = 'w';
+				break;
+			default:
+				console.log('Usage: git_hooks [--allow-update]');
+				process.exit(1);
+		}
+	}
+
+	installHook(config);
+}
+
+run(process.argv.slice(2));
