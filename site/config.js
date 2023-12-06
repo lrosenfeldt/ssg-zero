95df6edf6b7ee3e5003950ffec13be32342e4a1f
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-12-06 14:00:03 +0100

    feat: use pug to build home page

--- site/config.js
+++ site/config.js
@@ -1,13 +1,23 @@
+import * as pug from 'pug';
 import { SSGBuilder, LogLevel } from 'ssg-zero';
 
 const builder = new SSGBuilder();
 
+/**
+ * @type {import('ssg-zero').Renderer}
+ */
+const pugRenderer = {
+  generates: '.html',
+  render(content, data) {
+    const template = pug.compile(content, { pretty: true });
+    return template(data);
+  }
+}
+
 export default builder
 	.setInputDir('pages')
-	.setOutputDir('web')
+	.setOutputDir('www')
 	.useDefaultLogger(LogLevel.Debug)
-	.template('.html', content => {
-		console.log(content);
-		return content;
-	})
+  .template('.pug', pugRenderer)
+	.passthrough('.html')
 	.build();
