b1a051bccf20b8e3201b6286afed87226a11e74f
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-02-24 13:00:40 +0100

    feat: add watcher

---
+++ ssg-zero/src/usefule/watcher.test.ts
@@ -0,0 +1,98 @@
+import assert from 'node:assert/strict';
+import { beforeEach, describe as suite, test } from 'node:test';
+
+import { mkdir, rm, writeFile } from 'node:fs/promises';
+
+import { WatchEvent, Watcher, watch } from './watcher.js';
+
+suite('watcher initialization', function () {
+	test('fails if not initialized', async function () {
+		const watcher = new Watcher('fixtures/watch_static', 0);
+		await assert.rejects(async () => {
+			for await (const _ of watcher.watch()) {
+				break;
+			}
+		});
+	});
+});
+
+suite('watch events', async function () {
+	const baseDir = 'fixtures/watch';
+	const watcher = new Watcher(baseDir, 50);
+
+	const files = {
+		create: {
+			filePath: 'fixtures/watch/create.json',
+			content: '{"json":"jsml"}',
+		},
+		change: {
+			filePath: 'fixtures/watch/change.json',
+			contentBefore: '{"value":68}',
+			contentAfter: '{"value":69,"msg":"nice"}',
+		},
+		delete: {
+			filePath: 'fixtures/watch/delete.json',
+			content: '[]',
+		},
+	};
+
+	beforeEach(async () => {
+		await rm(baseDir, { recursive: true, force: true });
+		await mkdir(baseDir);
+
+		const p1 = writeFile(
+			files.change.filePath,
+			files.change.contentBefore,
+			'utf-8',
+		);
+		const p2 = writeFile(
+			files.delete.filePath,
+			files.delete.content,
+			'utf-8',
+		);
+
+		await Promise.all([p1, p2]);
+	});
+
+	const changeFs = async () => {
+		const p1 = writeFile(
+			files.create.filePath,
+			files.create.content,
+			'utf-8',
+		);
+		const p2 = writeFile(
+			files.change.filePath,
+			files.change.contentAfter,
+			'utf-8',
+		);
+		const p3 = rm(files.delete.filePath);
+
+		await Promise.all([p1, p2, p3]);
+	};
+
+	await test('emits the correct events', async function () {
+		await watcher.init();
+		const timeout = 5_000;
+    const expected: WatchEvent[] = [
+      { type: "create", filePath: files.create.filePath, content: files.create.content },
+      { type: "change", filePath: files.change.filePath, content: files.change.contentAfter },
+      { type: "delete", filePath: files.delete.filePath },
+    ];
+		let events: Set<WatchEvent> = new Set();
+
+    changeFs().catch(() => assert.fail(`couldn\'t make changes to base dir ${baseDir}`))
+
+		let start = performance.now();
+		for await (const event of watcher.watch()) {
+			events.add(event);
+			const delta = performance.now() - start;
+
+			if (events.size >= 3 || delta >= timeout) {
+				break;
+			}
+		}
+
+    assert.equal(events.size, 3);
+    assert.deepEqual(events, new Set(expected));
+	});
+});
---
+++ ssg-zero/src/usefule/watcher.ts
@@ -0,0 +1,129 @@
+import { opendir, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { setTimeout as sleep } from 'node:timers/promises';
+
+import { anyToError } from './core.js';
+
+export type WatchChangeEvent = {
+	type: 'change';
+	filePath: string;
+	content: string;
+};
+
+export type WatchCreateEvent = {
+	type: 'create';
+	filePath: string;
+	content: string;
+};
+
+export type WatchDeleteEvent = {
+	type: 'delete';
+	filePath: string;
+};
+
+export type WatchEvent = WatchChangeEvent | WatchCreateEvent | WatchDeleteEvent;
+
+export type Hash = string;
+
+export type WatcherOptions = {
+	maxInterval?: number;
+};
+
+export class Watcher {
+	private cache: Map<string, Hash> = new Map();
+	private initialized: boolean = false;
+
+	constructor(
+		private target: string,
+		private maxInterval: number,
+	) {}
+
+	async init(): Promise<void> {
+		this.cache = new Map();
+
+		const dir = await opendir(this.target, { recursive: true });
+		for await (const entry of dir) {
+			if (!entry.isFile()) continue;
+			// @ts-expect-error parentPath is to new for @types/node
+			const filePath = join(entry.parentPath, entry.name);
+			const content = await this.safeReadFile(filePath);
+			if (content !== undefined) {
+				this.cache.set(filePath, content);
+			}
+		}
+
+		this.initialized = true;
+	}
+
+	async *watch(): AsyncGenerator<WatchEvent> {
+		if (!this.initialized) {
+			throw new Error('Can not watch before initialization');
+		}
+
+		try {
+			while (true) {
+				const start = performance.now();
+
+				yield* this.watchGeneration();
+
+				const delta = performance.now() - start;
+				const delay = Math.max(this.maxInterval - delta, 0);
+
+				await sleep(delay);
+			}
+		} finally {
+			this.initialized = false;
+		}
+	}
+
+	private async *watchGeneration(): AsyncGenerator<WatchEvent> {
+		const dir = await opendir(this.target, { recursive: true });
+		const snapshot = this.cache;
+		this.cache = new Map();
+
+		for await (const entry of dir) {
+			if (!entry.isFile()) continue;
+			// @ts-expect-error parentPath is to new for @types/node
+			const filePath = join(entry.parentPath, entry.name);
+			const content = await this.safeReadFile(filePath);
+			// if file was deleted between discovery and now, just ignore that
+			if (content === undefined) continue;
+			const cachedContent = snapshot.get(filePath);
+
+			if (cachedContent === undefined) {
+				yield { type: 'create', filePath, content };
+			} else if (cachedContent !== content) {
+				yield { type: 'change', filePath, content };
+			}
+			this.cache.set(filePath, content);
+		}
+
+		for (const [filePath, _] of snapshot) {
+			if (!this.cache.has(filePath)) {
+				yield { type: 'delete', filePath };
+			}
+		}
+	}
+
+	private async safeReadFile(path: string): Promise<string | undefined> {
+		try {
+			return await readFile(path, 'utf-8');
+		} catch (reason) {
+			const error = anyToError(reason);
+			if (error.code === 'ENOENT') {
+				return undefined;
+			}
+			throw error;
+		}
+	}
+}
+
+export async function* watch(
+	target: string,
+	options: WatcherOptions = {},
+): AsyncGenerator<WatchEvent> {
+	const watcher = new Watcher(target, options.maxInterval ?? 500);
+
+	await watcher.init();
+	yield* watcher.watch();
+}
