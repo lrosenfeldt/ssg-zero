3a8d1c8b0ece09d35a71175791f65f55174cd276
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-03-16 21:08:14 +0100

    dont rename describe from node:test and avoid nesting of describe

--- ssg-zero/src/frontmatter.test.ts
+++ ssg-zero/src/frontmatter.test.ts
@@ -1,41 +1,29 @@
 import assert from 'node:assert/strict';
-import { describe as suite, test } from 'node:test';
+import { describe, test } from 'node:test';
 
-import {
-	type ParserResult,
-	UnexpectedEndOfJsonError,
-	parse,
-} from './frontmatter.js';
+import { UnexpectedEndOfJsonError, parse } from './frontmatter.js';
 
-suite('parseFrontmatter', function () {
-	suite('given en empty text', function () {
+describe('parseFrontmatter', function () {
+	test('returns an empty text and data for empty text', function () {
 		const text = '';
 		const result = parse(text);
-
-		test('returns an empty string as content when given an empty text', function () {
-			assert.equal(result.content, text);
-		});
-		test('returns no data when given an empty text', function () {
-			assert.equal(result.data, undefined);
-		});
+		assert.equal(result.content, text);
+		assert.equal(result.data, undefined);
 	});
-	suite('given no frontmatter', function () {
+
+	test('returns the full text and no data when given no frontmatter', function () {
 		const text = `\
-Roses are red
-Violets are blue
+        Roses are red
+      Violets are blue
 
-Unexpected '{'
-at line 32
-`;
+      Unexpected '{'
+        at line 32
+        `;
 		const result = parse(text);
-
-		test('returns the full text when given no frontmatter', function () {
-			assert.equal(result.content, text);
-		});
-		test('returns no data when given no frontmatter', function () {
-			assert.equal(result?.data, undefined);
-		});
+		assert.equal(result.content, text);
+		assert.equal(result?.data, undefined);
 	});
+
 	test('ignores frontmatter if preceeded by empty lines', function () {
 		const text = `\
 
@@ -72,10 +60,8 @@ at line 32
 			new UnexpectedEndOfJsonError(text, 0),
 		);
 	});
-});
 
-suite('given valid frontmatter', function () {
-	test('parses content and fronmatter', async function (t) {
+	test('parses content and a simple fronmatter', async function () {
 		const text = `\
 {
   "number": 69,
@@ -87,44 +73,33 @@ suite('given valid frontmatter', function () {
   }
 }
 Actual content`;
-		let result: ParserResult;
-		assert.doesNotThrow(() => {
-			result = parse(text);
+
+		const { data, content } = parse(text);
+
+		assert.equal(content, 'Actual content');
+		assert.deepEqual(data, {
+			number: 69,
+			bool: false,
+			text: 'The quick whatever does the thing.',
+			list: [1, 2, 4, 8, 16],
+			nested: {
+				abc: 123,
+			},
 		});
-		await t.test('strips the frontmatter from the text', function () {
-			assert.equal(result.content, 'Actual content');
-		});
-		await t.test('contains the correct data', function () {
-			const data = result.data;
-			assert.deepEqual(data, {
-				number: 69,
-				bool: false,
-				text: 'The quick whatever does the thing.',
-				list: [1, 2, 4, 8, 16],
-				nested: {
-					abc: 123,
-				},
-			});
-		});
-	});
-	test('parses empty frontmatter and content', async function (t) {
-		const text = `\
+
+		test('parses empty frontmatter and content', async function () {
+			const text = `\
 {
 }
 <p>Someday everything will be alright</p>
 `;
-		let result: ParserResult;
-		assert.doesNotThrow(() => {
-			result = parse(text);
-		});
-		await t.test('strips frontmatter from the text', function () {
+			const { content, data } = parse(text);
+
 			assert.equal(
-				result.content,
+				content,
 				'<p>Someday everything will be alright</p>\n',
 			);
-		});
-		await t.test('contains empty data', function () {
-			assert.deepEqual(result.data, {});
+			assert.deepEqual(data, {});
 		});
 	});
 });
