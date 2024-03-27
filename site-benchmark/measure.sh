9ac672670ff77b7c6c9725aabd6d11c8dce9067c
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-03-27 21:23:30 +0100

    refactor: make measurement script more adaptable

--- site-benchmark/measure.sh
+++ site-benchmark/measure.sh
@@ -2,29 +2,32 @@
 
 set -eu
 
-MACHINE="$(uname -sm | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
-DATE="$(date '+%y%m%d_%H%M%S')"
-BRANCH="$(git branch --show-current | tr '/' '.')"
-PREFIX="${MACHINE}_${BRANCH}_${DATE}"
-
-EXE_NODE="node"
-EXE_NPM="npm"
-RM="rm"
+OPT_INCLUDE_TIMESTAMP=""
+OPT_SWITCH_BRANCH=""
+OPT_FORCE=""
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
   -h | --help)
     echo "\
-Usage: measure [-h | --help] [-n | --dry]
+Usage: measure.sh [-h | --help] [-b | --branch <branch>] [-t | --timestamp] [-f | --force]
 Record heap, cpu and v8 profile for the current ssg-zero setup.
-  -h, --help  Show this message
-  -n, --dry   Only show what would be executed"
+  -h, --help        Show this message
+  -b, --branch      Branch to checkout before running
+  -t, --timestamp   Include a timestamp in file names
+  -f, --force       Overwrite results
+"
     exit 0
   ;;
-  -n | --dry)
-    RM="echo rm"
-    EXE_NODE="echo node"
-    EXE_NPM="echo npm"
+  -t | --timestamp)
+    OPT_INCLUDE_TIMESTAMP=true
+  ;;
+  -b | --branch)
+    shift
+    OPT_SWITCH_BRANCH="$1"
+  ;;
+  -f | --force)
+    OPT_FORCE=true
   ;;
   *)
     echo "Unknown option '$1'" 1>&2
@@ -35,19 +38,41 @@ Record heap, cpu and v8 profile for the current ssg-zero setup.
   shift
 done
 
-$EXE_NPM run --include-workspace-root --if-present compile
-$EXE_NODE --heap-prof --heap-prof-name="${PREFIX}.heapprofile.json" --diagnostic-dir=diagnostic zero.config.js
-$EXE_NODE --cpu-prof --cpu-prof-name="${PREFIX}.cpuprofile.json" --diagnostic-dir=diagnostic zero.config.js
-$EXE_NODE --prof zero.config.js
-
-V8_FILE=""
-if [ "$EXE_NODE" == "node" ]; then
-  V8_FILE="$(ls -t1 | awk '/isolate/' | head -n 1)"
-  [ -f "$V8_FILE" ] || exit 1
-else
-  V8_FILE="V8_FILE"
+MACHINE="$(uname -sm | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
+DATE=""
+BRANCH=""
+if [ -n "$OPT_INCLUDE_TIMESTAMP" ]; then
+  DATE="_$(date '+%y%m%d_%H%M%S')"
 fi
 
-$EXE_NODE --prof-process "$V8_FILE" > "diagnostic/${PREFIX}.v8profile.txt"
+if [ -n "$OPT_SWITCH_BRANCH" ]; then
+  if ! git checkout "$OPT_SWITCH_BRANCH" 1>/dev/null 2>/dev/null; then
+    exit 8
+  fi
+fi
 
-$RM "$V8_FILE"
+BRANCH="_$(git branch --show-current | tr '/' '.')"
+PREFIX="${MACHINE}${BRANCH}${DATE}"
+
+if [ -z "$OPT_FORCE" ]; then
+  [ -f "./diagnostic/${PREFIX}.v8profile.txt" ] && exit 16
+  [ -f "./diagnostic/${PREFIX}.heapprofile.json" ] && exit 16
+  [ -f "./diagnostic/${PREFIX}.cpuprofile.json" ] && exit 16
+fi
+
+npm run --include-workspace-root --if-present compile
+node --heap-prof --heap-prof-name="${PREFIX}.heapprofile.json" --diagnostic-dir=diagnostic zero.config.js
+node --cpu-prof --cpu-prof-name="${PREFIX}.cpuprofile.json" --diagnostic-dir=diagnostic zero.config.js
+node --prof zero.config.js
+
+V8_FILE="$(ls -t1 | awk '/isolate/' | head -n 1)"
+if ! [ -f "$V8_FILE" ]; then
+ exit 1
+fi
+
+node --prof-process "$V8_FILE" > "diagnostic/${PREFIX}.v8profile.txt"
+
+rm "$V8_FILE"
+if [ -n "$OPT_SWITCH_BRANCH" ]; then
+  git checkout -
+fi
