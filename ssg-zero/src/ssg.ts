137a968d9dcd298c3c2984aa4c4a9cc9fb1fe582
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-03-29 22:09:43 +0100

    refactor: power build process by a nodejs stream

--- ssg-zero/src/ssg.ts
+++ ssg-zero/src/ssg.ts
@@ -1,10 +1,14 @@
 import { basename, dirname, extname, join } from 'node:path';
+import { Writable } from 'node:stream';
+import { cp, mkdir, readFile, writeFile } from 'node:fs';
+import { mkdir as mkdirAsync } from 'node:fs/promises';
 
-import { cp, mkdir, readFile, writeFile } from 'node:fs/promises';
-
-import { FileReader, walkFiles } from './usefule/core.js';
+import { FileReader, anyToError, walkFiles } from './usefule/core.js';
 import { logger, type Logger } from './logger.js';
-import { parseFrontmatter } from './frontmatter.js';
+import { ParserResult, parseFrontmatter } from './frontmatter.js';
+import { pipeline } from 'node:stream/promises';
+
+type DoneCallback = (error?: NodeJS.ErrnoException | null | undefined) => void;
 
 const passthroughMarker = Symbol('passthrough');
 export type PassthroughMarker = typeof passthroughMarker;
@@ -21,7 +25,7 @@ export type Renderer = {
 
 export type FileHandler = PassthroughMarker | Renderer;
 
-export class SSG {
+export class SSG extends Writable {
 	static passthroughMarker: PassthroughMarker = passthroughMarker;
 
 	constructor(
@@ -30,73 +34,55 @@ export class SSG {
 		public readonly includesDir: string,
 		public readonly fileHandlers: Readonly<Record<string, FileHandler>>,
 		private logger: Logger,
-	) {}
+	) {
+		super({ objectMode: true, highWaterMark: 20 });
+	}
 
 	async setup(): Promise<void> {
-		await mkdir(this.outputDir, { recursive: true });
+		await mkdirAsync(this.outputDir, { recursive: true });
 	}
 
-	async build(reader: FileReader = walkFiles(this.inputDir)): Promise<void> {
-		for await (const inputFilePath of reader) {
-			await this.handleFile(inputFilePath);
+	_write(
+		filePath: any,
+		_encoding: BufferEncoding,
+		done: (error?: Error | null | undefined) => void,
+	): void {
+		if (typeof filePath !== 'string') {
+			done(
+				new Error(
+					`Bad configured writable, got chunk of type ${typeof filePath} expected a string.`,
+				),
+			);
+			return;
 		}
-	}
 
-	private async handleFile(inputFilePath: string): Promise<void> {
-		const fileType = extname(inputFilePath);
+		const fileType = extname(filePath);
 		this.logger.trace('Found file', {
-			file: inputFilePath,
+			file: filePath,
 			ext: fileType,
 		});
 
 		const renderer = this.getFileHandler(fileType);
 		if (renderer === undefined) {
 			this.logger.info('Ignore file', {
-				file: inputFilePath,
+				file: filePath,
 				ext: fileType,
 			});
-			return;
+			done();
+		} else if (renderer === SSG.passthroughMarker) {
+			this.passthroughFile(filePath, done);
+		} else {
+			this.handleTemplate(filePath, fileType, renderer, done);
 		}
-
-		if (renderer === SSG.passthroughMarker) {
-			await this.passthroughFile(inputFilePath);
-			return;
-		}
-
-		// render
-		await this.renderTemplate(inputFilePath, fileType, renderer);
 	}
 
-	private getLayoutRendererOrFail(
-		filePath: string,
-		data: {
-			layout: any;
-		},
-	): Renderer {
-		if (typeof data.layout !== 'string') {
-			const error = new Error(
-				`Invalid layout used in ${filePath}, expected a string got '${typeof data.layout}'`,
-			);
-			this.logger.error(error.message, {
-				template: filePath,
-				layout: data.layout,
-			});
-			throw error;
+	async build(reader: FileReader = walkFiles(this.inputDir)): Promise<void> {
+		try {
+			await pipeline(reader, this);
+		} catch (reason) {
+			const error = anyToError(reason);
+			this.logger.error(error);
 		}
-
-		const layoutExtension = extname(data.layout);
-		const handler = this.getFileHandler(layoutExtension);
-		if (handler === undefined || handler === passthroughMarker) {
-			const error = new Error(
-				`Invalid layout used in ${filePath}, render is ${handler === undefined ? 'missing' : 'can only copy'}`,
-			);
-			this.logger.error(error.message, {
-				template: filePath,
-				layoutExtension,
-			});
-			throw error;
-		}
-		return handler;
 	}
 
 	private getFileHandler(extension: string): FileHandler | undefined {
@@ -106,25 +92,12 @@ export class SSG {
 		return this.fileHandlers[extension];
 	}
 
-	private async passthroughFile(filePath: string): Promise<void> {
-		const outputFilePath = filePath.replace(this.inputDir, this.outputDir);
-		this.logger.info('Copying file', {
-			file: filePath,
-			to: outputFilePath,
-		});
-
-		await cp(filePath, outputFilePath, {
-			force: true,
-			recursive: true,
-		});
-		return;
-	}
-
-	private async renderTemplate(
+	private handleTemplate(
 		filePath: string,
 		fileType: string,
 		renderer: Renderer,
-	): Promise<void> {
+		done: DoneCallback,
+	) {
 		// setup dir
 		const targetDir = dirname(filePath).replace(
 			this.inputDir,
@@ -134,60 +107,183 @@ export class SSG {
 			file: filePath,
 			dir: targetDir,
 		});
-		await mkdir(targetDir, { recursive: true });
+		mkdir(targetDir, { recursive: true }, error => {
+			if (error) {
+				done(error);
+				return;
+			}
 
-		const targetFile = join(
-			targetDir,
-			basename(filePath).replace(fileType, renderer.generates),
-		);
-		this.logger.info('Rendering', {
-			file: filePath,
-			to: targetFile,
+			const targetPath = join(
+				targetDir,
+				basename(filePath).replace(fileType, renderer.generates),
+			);
+			this.logger.info('Rendering', {
+				file: filePath,
+				to: targetPath,
+			});
+			readFile(filePath, 'utf-8', (error, content) => {
+				if (error) {
+					done(error);
+					return;
+				}
+
+				try {
+					const parsed = parseFrontmatter(content);
+					this.renderTemplate(
+						filePath,
+						targetPath,
+						renderer,
+						parsed,
+						done,
+					);
+				} catch (error) {
+					done(anyToError(error));
+				}
+			});
 		});
-		const fullContent = await readFile(filePath, 'utf-8');
-		const parsedContent = parseFrontmatter(fullContent);
+	}
 
-		let output: string;
+	private passthroughFile(filePath: string, done: DoneCallback): void {
+		const outputFilePath = filePath.replace(this.inputDir, this.outputDir);
+		this.logger.info('Copying file', {
+			file: filePath,
+			to: outputFilePath,
+		});
+
+		cp(
+			filePath,
+			outputFilePath,
+			{
+				force: true,
+				recursive: true,
+			},
+			done,
+		);
+	}
+
+	private renderTemplate(
+		filePath: string,
+		targetPath: string,
+		renderer: Renderer,
+		parsed: ParserResult,
+		done: DoneCallback,
+	): void {
 		if (
-			parsedContent.data !== undefined &&
-			'layout' in parsedContent.data
+			parsed.data !== undefined &&
+			'layout' in parsed.data &&
+			parsed.data.layout !== undefined
 		) {
-			const layoutRenderer = this.getLayoutRendererOrFail(
-				filePath,
-				parsedContent.data,
-			);
-			const layoutFilePath = join(
-				this.includesDir,
-				parsedContent.data.layout as string,
-			);
-			const layoutContent = await readFile(layoutFilePath, 'utf-8');
-			const renderedContent = await renderer.render(
-				parsedContent.content,
-				parsedContent.data,
-				{ input: filePath, output: targetFile },
-			);
-			output = await layoutRenderer.render(
-				layoutContent,
-				{
-					...parsedContent.data,
-					layout: undefined,
-					content: renderedContent,
+			if (typeof parsed.data.layout !== 'string') {
+				const error = new Error(
+					`Invalid layout used in ${filePath}, expected a string got '${typeof parsed.data.layout}'`,
+				);
+				done(error);
+				return;
+			}
+
+			const layoutExtension = extname(parsed.data.layout);
+			const handler = this.getFileHandler(layoutExtension);
+			if (handler === undefined || handler === SSG.passthroughMarker) {
+				const error = new Error(
+					`Invalid layout used in ${filePath}, render is ${handler === undefined ? 'missing' : 'can only copy'}`,
+				);
+				done(error);
+				return;
+			}
+
+			const layoutPath = join(this.includesDir, parsed.data.layout);
+			this.runRenderer(
+				renderer,
+				parsed.content,
+				parsed.data,
+				{ input: filePath, output: targetPath },
+				(error, content) => {
+					if (error) {
+						done(error);
+						return;
+					}
+
+					readFile(layoutPath, 'utf-8', (error, layoutContent) => {
+						if (error) {
+							done(error);
+							return;
+						}
+
+						this.renderTemplate(
+							filePath,
+							targetPath,
+							handler,
+							{
+								content: layoutContent,
+								data: {
+									...parsed.data,
+									layout: undefined,
+									content,
+								},
+							},
+							done,
+						);
+					});
 				},
-				{ input: layoutFilePath, output: targetFile },
 			);
-		} else {
-			output = await renderer.render(
-				parsedContent.content,
-				parsedContent.data,
-				{ input: filePath, output: targetFile },
-			);
+			return;
+		}
+		const write = (
+			error: NodeJS.ErrnoException | null | undefined,
+			output: string,
+		) => {
+			if (error) {
+				done(error);
+				return;
+			}
+			writeFile(targetPath, output, 'utf-8', error => {
+				if (error) {
+					done(error);
+					return;
+				}
+				this.logger.debug('Rendering done', {
+					file: filePath,
+					to: targetPath,
+				});
+				done();
+			});
+		};
+
+		this.runRenderer(
+			renderer,
+			parsed.content,
+			parsed.data,
+			{ input: filePath, output: targetPath },
+			write,
+		);
+	}
+
+	private runRenderer(
+		renderer: Renderer,
+		template: string,
+		data: any,
+		meta: Parameters<RenderFn>[2],
+		callback: (
+			error: NodeJS.ErrnoException | null | undefined,
+			output: string,
+		) => void,
+	): void {
+		let rendered: string | Promise<string>;
+		try {
+			rendered = renderer.render(template, data, meta);
+		} catch (reason) {
+			const error = anyToError(reason);
+			callback(error, '');
+			return;
 		}
 
-		await writeFile(targetFile, output, 'utf-8');
-		this.logger.debug('Rendering done', {
-			file: filePath,
-			to: targetFile,
-		});
+		if (rendered instanceof Promise) {
+			rendered
+				.then(output => callback(undefined, output))
+				.catch(reason => callback(anyToError(reason), ''));
+		} else {
+			callback(undefined, rendered);
+		}
 	}
 }
 
